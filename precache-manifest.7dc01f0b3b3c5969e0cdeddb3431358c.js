6c4a130d6ed25dd7342546695070edc6c993b573
tinfoil-knight <kunal99kundu@gmail.com>
2020-07-24 20:04:28 +0530

    Updates

---
+++ precache-manifest.7dc01f0b3b3c5969e0cdeddb3431358c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87699b568153c8c2025144c411236ffe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d75cd46f8b04d34bbcd0",
+    "url": "./static/css/main.1c98fcb6.chunk.css"
+  },
+  {
+    "revision": "0ad1cbf06d183c50075b",
+    "url": "./static/js/2.db20aaf7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.db20aaf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75cd46f8b04d34bbcd0",
+    "url": "./static/js/main.15f32721.chunk.js"
+  },
+  {
+    "revision": "2d59e916d8fc3ce6d479",
+    "url": "./static/js/runtime-main.4642b631.js"
+  }
+]);
