b3c7766d7de803e31e407bd5bfe025cd8a142ddc
tinfoil-knight <kunal99kundu@gmail.com>
2020-07-27 13:46:00 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35cbcb09d154e30b53c1e9ad44360df3.js"
+  "./precache-manifest.62e0e195a66d284071a1995d995a351f.js"
 );
 
 self.addEventListener('message', (event) => {
